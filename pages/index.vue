570012b0768c3792f94183d4ca6553a15a2db0a7
admin@dbsgw.cn <admin@dbsgw.cn>
2022-04-15 12:51:24 +0800

    5
    Signed-off-by: admin@dbsgw.cn <admin@dbsgw.cn>

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="name">
-    <div class="age">
-      首页
-    </div>
+  <div class="rust_home">
+    5454
   </div>
 </template>
 
@@ -26,6 +24,9 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.rust_home {
+  background-color: var(--color-sub-background);
+  padding: 20px;
+}
 </style>
